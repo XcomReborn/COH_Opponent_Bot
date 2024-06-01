ef698c5e6eefbf308ab0ab5d33eea45618a6a847
XcomReborn <xcom_commander@yahoo.com>
2024-06-01 08:42:04 +0100

    GUI layout changes

--- classes/oppbot_gui_options_overlay.py
+++ classes/oppbot_gui_options_overlay.py
@@ -373,52 +373,11 @@ class OptionsOverlay:
             command=lambda : self.mirror_left_right_entry(self.entry_custom_pf_right))
         self.button_mirror_custom_pf.grid(sticky=E, row=6, column=3)
 
-        # setdisabled if custom format on first run
-        # self.toggle_use_overlay_preformat()
-        # self.automode() # setdisabled if auto on first run
-
-        # Stats Display PreFormat
-
-        self.frame_stats_pf = tkinter.LabelFrame(
-                self.frame,
-                text="Stats Display Preformat Text",
-                padx=5,
-                pady=5)
-
-        self.frame_stats_pf.grid(sticky=N+S+W+E)
-        self.frame_stats_pf.grid_rowconfigure(0, weight=1)
-        self.frame_stats_pf.grid_columnconfigure(0, weight=1)
-
-        self.textbox_stats_pf = tkinter.Text(
-            self.frame_stats_pf,
-            height=10)
-        self.textbox_stats_pf.grid(sticky="nsew", row=1, column=0, padx=2, pady=2)
-
-        text = ""
-        if self.settings.data.get("overlay_display_pf"):
-            text = self.settings.data.get("overlay_display_pf")
-
-        self.textbox_stats_pf.insert(END, text)
-
-        # create a Scrollbar and associate it with txt
-        scrollbar = ttk.Scrollbar(self.frame_stats_pf, command=self.textbox_stats_pf.yview)
-        scrollbar.grid(sticky='nsew', row=1, column=1)
-        self.textbox_stats_pf['yscrollcommand'] = scrollbar.set
-
-        # save button
-
-        self.button_save = tkinter.Button(
-            self.frame_stats_pf,
-            text="Save",
-            command=self.save_display
-        )
-        self.button_save.grid()
-
         # CSS File Location Frame
 
         self.frame_css_file_path = tkinter.LabelFrame(
             self.frame,
-            text="CSS Format File",
+            text="CSS Format Files",
             padx=5,
             pady=5)
         self.frame_css_file_path.grid(sticky=N+W+E)
@@ -519,18 +478,65 @@ class OptionsOverlay:
         self.button_css_unranked_path.config(width=10)
         self.button_css_unranked_path.grid(row=2, column=2, sticky=W)
 
-        # CSS Label
+        # Stats Display PreFormat
+
+        self.frame_stats_pf = tkinter.LabelFrame(
+                self.frame,
+                text="Stats Display Preformat Text",
+                padx=5,
+                pady=5)
+
+        self.frame_stats_pf.grid(sticky=N+S+W+E)
+        self.frame_stats_pf.grid_rowconfigure(0, weight=1)
+        self.frame_stats_pf.grid_columnconfigure(0, weight=1)
+
+        self.textbox_stats_pf = tkinter.Text(
+            self.frame_stats_pf,
+            height=10)
+        self.textbox_stats_pf.grid(sticky="nsew", row=1, column=0, padx=2, pady=2)
+
+        text = ""
+        if self.settings.data.get("overlay_display_pf"):
+            text = self.settings.data.get("overlay_display_pf")
+
+        self.textbox_stats_pf.insert(END, text)
+
+        # create a Scrollbar and associate it with txt
+        scrollbar = ttk.Scrollbar(self.frame_stats_pf, command=self.textbox_stats_pf.yview)
+        scrollbar.grid(sticky='nsew', row=1, column=1)
+        self.textbox_stats_pf['yscrollcommand'] = scrollbar.set
+
+        # save button
+
+        self.button_save = tkinter.Button(
+            self.frame_stats_pf,
+            text="Save",
+            command=self.save_display
+        )
+        self.button_save.grid()
+
+        # CSS File Location Stats Frame
+
+        self.frame_css_file_path_display = tkinter.LabelFrame(
+            self.frame,
+            text="CSS Format File",
+            padx=5,
+            pady=5)
+        self.frame_css_file_path_display.grid(sticky=N+W+E)
+
+
+        # CSS Label Display
 
         self.label_css_display = tkinter.Label(
-            self.frame_css_file_path,
+            self.frame_css_file_path_display,
             text="Stat Display CSS Path"
         )
         self.label_css_display.grid(row=3, column=0, sticky=W)
 
-        # CSS Entry
+        # CSS Entry Display
 
         self.entry_css_display_file_path = tkinter.Entry(
-            self.frame_css_file_path,
+            self.frame_css_file_path_display,
             width=49
         )
         self.entry_css_display_file_path.grid(row=3, column=1)
@@ -542,10 +548,10 @@ class OptionsOverlay:
 
         self.entry_css_display_file_path.config(state=DISABLED)
 
-        # CSS Browse Button
+        # CSS Browse Button Display
 
         self.button_css_custom_path = tkinter.Button(
-            self.frame_css_file_path,
+            self.frame_css_file_path_display,
             text="Browse",
             command=self.browse_css_display_file_path_button)
         self.button_css_custom_path.config(width=10)
