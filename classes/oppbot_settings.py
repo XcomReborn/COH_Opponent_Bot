2fd570db86f54ee81319c4929e06541e2d65b0b9
XcomReborn <xcom_commander@yahoo.com>
2024-06-18 16:04:00 +0100

    updated version number

--- classes/oppbot_settings.py
+++ classes/oppbot_settings.py
@@ -21,8 +21,8 @@ class Settings:
         self.privatedata = {}
 
         # Manually update for about box.
-        self.privatedata['version_number'] = "5.2.5"
-        self.privatedata['build_date'] = "17-JUN-2024"
+        self.privatedata['version_number'] = "5.2.6"
+        self.privatedata['build_date'] = "18-JUN-2024"
 
         # custom display toggles
         # what to show in stat string constuct
