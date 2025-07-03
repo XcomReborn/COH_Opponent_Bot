0387fe7ae3eca19f464aa8193aa2a2d4decbc471
XcomReborn <zachary.coldrick@gmail.com>
2025-07-03 17:37:08 +0100

    Update oppbot_http_server.py
    Added QuietRequestHandler to move http connection log messages into logging info instead of console.

--- classes/oppbot_http_server.py
+++ classes/oppbot_http_server.py
@@ -1,4 +1,5 @@
 import http.server
+import itertools
 import threading
 import logging
 
@@ -11,7 +12,7 @@ class OppBotHttpServer(threading.Thread):
         """Initialize the server with the specified host and port."""
         threading.Thread.__init__(self)
         self.server_address = (host, port)
-        self.RequestHandler = http.server.SimpleHTTPRequestHandler
+        self.RequestHandler = QuietRequestHandler
         self.httpd = http.server.HTTPServer(self.server_address, self.RequestHandler)
 
     def run(self):
@@ -26,6 +27,34 @@ class OppBotHttpServer(threading.Thread):
         self.httpd.server_close()
         logging.info("Server stopped.")
 
+
+class QuietRequestHandler(http.server.SimpleHTTPRequestHandler):
+    """A custom request handler that suppresses logging for GET requests."""
+    
+    def log_message(self, format, *args):
+        """Override to suppress logging of GET requests to console."""
+        message = format % args
+        logging.info("%s - - [%s] %s\n" %
+                         (self.address_string(),
+                          self.log_date_time_string(),
+                          message.translate(self._control_char_table)))
+    # https://en.wikipedia.org/wiki/List_of_Unicode_characters#Control_codes
+    _control_char_table = str.maketrans(
+            {c: fr'\x{c:02x}' for c in itertools.chain(range(0x20), range(0x7f,0xa0))})
+
+    # def do_GET(self):
+    #     """Handle GET requests."""
+    #     self.send_response(200)
+    #     self.send_header('Content-type', 'text/html')
+    #     self.end_headers()
+    #     self.wfile.write(b"Welcome to the OppBot HTML Server!")
+
+    def do_POST(self):
+        """Handle POST requests."""
+        self.send_response(501)  # Not Implemented
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"POST method not implemented.")
 
 if __name__ == "__main__":
 
